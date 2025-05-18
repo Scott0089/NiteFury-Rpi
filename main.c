abfb7f9b3e10d6e4f55786556f4fe99317ae8a2c
Scott0089 <james.howell525@gmail.com>
2025-05-18 00:27:18 -0400

    no clue

--- main.c
+++ main.c
@@ -22,12 +22,24 @@ int main()
     fd = open("/dev/xdma0_user", O_RDWR);
     if(fd < 0)
     {
-        perror("open\r\n");
-        return -1;
+        printf("Failed to open /dev/xdma0_user!\r\n");
+        return XST_FAILURE;
     }
 
-    int status;
+    uint32_t status;
+    uint32_t data = NULL;
 
+    status = XGpio_Initialize(&gpioInst[4], XPAR_AXI_GPIO_4_BASEADDR);
+    if (status != XST_SUCCESS) 
+    {
+        printf("GPIO Read failed to Init!\r\n");
+        return XST_FAILURE;
+    }
+
+    data = XGpio_DiscreteRead(&gpioInst[4], 0x01);
+
+    printf("\n\n\r     ** NiteFury RaspberryPi **\n\r");
+    printf("   FPGA Build/Version: 0x%08X\n\n\r", XGpio_DiscreteRead(&gpioInst[4], 1));
     
     for (size_t i = 0; i < 4; i++)
     {
@@ -38,7 +50,6 @@ int main()
             return XST_FAILURE;
         }
 
-        //gpioInst[i].BaseAddress = gpioInst[i].BaseAddress - AXI_Translation;
     }
 
     for (size_t i = 0; i < 4; i++)
@@ -46,18 +57,8 @@ int main()
         XGpio_DiscreteWrite(&gpioInst[i], 0x01, OFF);
     }
 
-    uint32_t data = NULL;
-    status = XGpio_Initialize(&gpioInst[4], XPAR_AXI_GPIO_4_BASEADDR);
-    if (status != XST_SUCCESS) 
-    {
-        printf("GPIO Read failed to Init!\r\n");
-        return XST_FAILURE;
-    }
-    
-    //gpioInst[4].BaseAddress = gpioInst[4].BaseAddress - AXI_Translation;
-
-    data = XGpio_DiscreteRead(&gpioInst[4], 0x01);
-    printf("Data: %x\r\n", data);
+    XGpio_DiscreteWrite(&gpioInst[2], 0x01, ON);
+    XGpio_DiscreteWrite(&gpioInst[3], 0x01, ON);
     
     status = XTmrCtr_Initialize(&tmrInst, XPAR_AXI_TIMER_0_BASEADDR);
     if (status != XST_SUCCESS) {
@@ -65,105 +66,36 @@ int main()
         return XST_FAILURE;
     }
 
-    
    	XTmrCtr_SetResetValue(&tmrInst, 0, (u32) -50000000);
    	XTmrCtr_Start(&tmrInst, 0);
+    
+    XTmrCtr_SetResetValue(&tmrInst, 1, (u32) -37500000);
+   	XTmrCtr_Start(&tmrInst, 1);
 
     uint8_t data2 = 0x00;
 
     while(1)
     {
+        if(XTmrCtr_IsExpired(&tmrInst, 1))
+        {
+
+            XTmrCtr_Reset(&tmrInst, 1);
+            data2 = XGpio_DiscreteRead(&gpioInst[1], 0x01);
+            XGpio_DiscreteWrite(&gpioInst[1], 0x01, !data2);
+            data2 = XGpio_DiscreteRead(&gpioInst[2], 0x01);
+            XGpio_DiscreteWrite(&gpioInst[2], 0x01, !data2);
+        }
+
         if(XTmrCtr_IsExpired(&tmrInst, 0))
         {
-             //printf("here\r\n");
-             XTmrCtr_Reset(&tmrInst, 0);
-             data2 = XGpio_DiscreteRead(&gpioInst[0], 0x01);
-             XGpio_DiscreteWrite(&gpioInst[0], 0x01, !data2);
+
+            XTmrCtr_Reset(&tmrInst, 0);
+            data2 = XGpio_DiscreteRead(&gpioInst[0], 0x01);
+            XGpio_DiscreteWrite(&gpioInst[0], 0x01, !data2);
+            data2 = XGpio_DiscreteRead(&gpioInst[3], 0x01);
+            XGpio_DiscreteWrite(&gpioInst[3], 0x01, !data2);
         }
     }
-
-
-
-    //XGpio_DiscreteWrite(&gpioInst[0], 0x01, ON);
-    /*
-    while (1)
-    {
-        for (size_t i = 0; i < 4; i++)
-        {
-            XGpio_DiscreteWrite(&gpioInst[i], 0x01, ON);
-            sleep(1);
-        }
-        for (size_t i = 0; i < 4; i++)
-        {
-            XGpio_DiscreteWrite(&gpioInst[3-i], 0x01, OFF);
-            sleep(1);
-        }
-    }
-
-
-    /*
-
-    status = XTmrCtr_Initialize(&tmrInst, XPAR_AXI_TIMER_0_BASEADDR);
-    if (status != XST_SUCCESS) 
-    {
-        printf("Timer failed to Init!\r\n");
-        return XST_FAILURE;
-    }
-    //tmrInst.BaseAddress = tmrInst.BaseAddress - AXI_Translation;
-    
-    printf("%x \r\n", tmrInst.BaseAddress);
-    //tmrInst.BaseAddress = XPAR_XTMRCTR_0_BASEADDR;
-    printf("%x \r\n", tmrInst.BaseAddress);
-
-    /*
-
-    status = XTmrCtr_SelfTest(&tmrInst, 0);
-    if (status != XST_SUCCESS) 
-    {
-        printf("Timer Self Test failed to Init!\r\n");
-        return XST_FAILURE;
-    }
-
-    */
-
-    //printf("%x \r\n", XPAR_AXI_TIMER_0_BASEADDR);
-    //printf("%x \r\n", tmrInst.BaseAddress);
-    /*
-    uint32_t Value1;
-    uint32_t Value2;
-
-    XTmrCtr_SetOptions(&tmrInst, TIMER_COUNTER_0, XTC_AUTO_RELOAD_OPTION);
-    Value1 = XTmrCtr_GetValue(&tmrInst, TIMER_COUNTER_0);
-
-    printf("%d \r\n", Value1);
-    XTmrCtr_Start(&tmrInst, TIMER_COUNTER_0);
-
-    while(1)
-    {
-        Value2 = XTmrCtr_GetValue(&tmrInst, TIMER_COUNTER_0);
-        //printf("Time: %d\r\n", Value2);
-        if (Value1 != Value2)
-        {
-            break;
-        }
-        
-    }
-
-    while (1)
-    {
-        for (size_t i = 0; i < 4; i++)
-        {
-            XGpio_DiscreteWrite(&gpioInst[i], 0x01, ON);
-            sleep(1);
-        }
-        for (size_t i = 0; i < 4; i++)
-        {
-            XGpio_DiscreteWrite(&gpioInst[3-i], 0x01, OFF);
-            sleep(1);
-        }
-    }
-
-    */
 
     printf("Everything done! \r\n Exiting... \r\n");
 
